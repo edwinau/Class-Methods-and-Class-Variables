5ceb04ee96b2a091fdbcf4cd48c8e02ed073052e
EDWIN AU <edwinau@rogers.com>
2017-04-01 15:00:39 -0400

    BookLending - complete

--- BookLending.rb
+++ BookLending.rb
@@ -1,9 +1,10 @@
 class Book
-  # attr_reader :on_shelf, :on_loan
-  # attr_accessor :due_date, :title, :author, :isbn
+  attr_reader :on_shelf, :on_loan
+  attr_accessor :due_date
 
   @@on_shelf = [] #contains collection for lent.
   @@on_loan = [] #contains collection borrowed.
+  @@due_date = {} #contains due dates
 
   def self.create(title, author, isbn)
     x = Book.new(title, author, isbn)
@@ -26,15 +27,21 @@ class Book
       return false
     end
     @@on_loan << self
-    # @@on_shelf >> self
-    due_date = "nil"
+    @@due_date[self] = Time.now + (3) #(60*60*24*5)
     return true
   end
 
-  def self.borrowed_array
-    @@on_loan
-    # @@on_shelf
-  end
+  # Code for debugging
+  # def self.borrowed_array
+  #   @@on_loan
+  #   # @@on_shelf
+  # end
+  #
+  # def self.duedate_array
+  #   @@due_date.each do |k,v|
+  #       puts v.to_s
+  #   end
+  # end
 
   def self.borrowed(book)
     if book.lent_out?
@@ -44,25 +51,45 @@ class Book
     end
   end
   #
-  # def return_to_library
-  #   @@_shelf < self
-  #   @@_loan >> self
-  # end
+  def return_to_library
+    if self.lent_out?
+      @@on_shelf << self
+      @@due_date.delete self
+      @@on_loan.delete self
+      puts "abc"
+      return true
+    else
+      return false
+    end
+  end
 
-  # def self.current_due_date
-  #
-  # end
-  #
-  # def self.overdue_books
-  # end
-  #
+  def self.current_due_date(book)
+    @@due_date.each do |k,v|
+      if k == book
+        puts "found book"
+        return v
+      end
+    end
+    return Time.now
+  end
+
+  def self.overdue_books
+    x = []
+    @@due_date.each do |k,v|
+      if v <=> Time.now
+        puts "#{k} + #{v}"
+        x << k
+      end
+    end
+    return x
+  end
+end
+
   # def self.browse
   #   @@on_shelf[rand]
   # end
-  #
+
   # def self.available
   #   @@on_shelf
   # end
-  #
-
-end
+  # #
